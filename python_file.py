edd44517e5dd1b67eca254cb5adde027aea37613
Jessie De Jong <131053169+JessieDeJong@users.noreply.github.com>
2024-04-04 14:50:13 +0200

    Create python_file.py
    Added a python_file.py with my code from the School Catalogue project.

---
+++ python_file.py
@@ -0,0 +1,74 @@
+class School:
+
+  def __init__(self, name, level, numberOfStudents=0):
+    self.name = name
+    self.level = level
+    self.numberOfStudents = numberOfStudents
+
+  def get_name(self):
+    return self.name
+  
+  def get_level(self):
+    return self.level
+
+  def get_numberOfStudents(self):
+    return self.numberOfStudents
+
+  def set_numberOfStudents(self, new_number):
+    self.numberOfStudents = new_number
+
+  def __repr__(self):
+    return f"This is a {self.level} school named {self.name} with {self.numberOfStudents} students."
+
+class PrimarySchool(School):
+
+  def __init__(self, name, numberOfStudents, pickUpPolicy):
+    super().__init__(name, "Primary", numberOfStudents)
+    self.pickUpPolicy = pickUpPolicy
+
+  def get_pickUpPolicy(self):
+    return self.pickUpPolicy
+
+  def __repr__(self):
+    return super().__repr__() + f"The PickUp Policy of this school is: {self.pickUpPolicy}"
+
+class MiddleSchool(School):
+
+  def __init__(self, name, numberOfStudents):
+    super().__init__(name, "Middle", numberOfStudents)
+
+class HighSchool(School):
+  
+  def __init__(self, name, numberOfStudents, sportsTeams):
+    super().__init__(name, "High", numberOfStudents)
+    self.sportsTeams = sportsTeams
+
+  def get_sportsTeams(self):
+    return self.sportsTeams
+
+  def __repr__(self):
+    list_sportsTeams = ""
+    for team in self.sportsTeams:
+      if team == self.sportsTeams[-1]:
+        list_sportsTeams += team + "."
+      else:
+        list_sportsTeams += team + ", "
+
+    return super().__repr__() + f"This school has these sports teams: {list_sportsTeams}" 
+
+Imaculata = School("Imaculata", "High")
+Imaculata.set_numberOfStudents(150)
+#print(Imaculata)
+
+a = PrimarySchool("Peuterbos", 50, "Pickup after 3pm.")
+#print(a)
+#print(a.get_numberOfStudents())
+
+b = MiddleSchool("Lorcana", 120)
+b.set_numberOfStudents(130)
+#print(b.get_name())
+#print(b)
+
+c = HighSchool("Codecademy High", 500, ["Tennis", "Basketball"])
+#print(c.get_sportsTeams())
+#print(c)
